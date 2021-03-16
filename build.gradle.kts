4c14ae75f780a21d876d9b34b06e40bdf339a41a
noyzys <damian.festowski@onet.pl>
2021-03-16 18:40:11 +0100

    shared on github

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+group = "io.github.noyzys.bukkit.location"
+version = "0.0.1-SNAPSHOT"
+
+plugins {
+    kotlin("jvm") version "1.5.0-M1"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    compileOnly(files("C:/Users/opel3/Desktop/Depends/PaperSpigot-1.8.8-R0.1-SNAPSHOT-latest.jar"))
+}
+
---
+++ src/main/kotlin/io/github/noyzys/bukkit/location/IImmutableLookable.kt
@@ -0,0 +1,7 @@
+package io.github.noyzys.bukkit.location
+
+/**
+ * @author: noyzys on 18:36, 16.03.2021
+ **/
+interface IImmutableLookable {
+}
